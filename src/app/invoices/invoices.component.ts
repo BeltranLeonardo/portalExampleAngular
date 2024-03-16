dbe50213744c5cf49d46bd9680cab051363045ec
lbeltran <lbeltran@enerser.com.mx>
2024-03-15 17:31:52 -0700

    commit

--- src/app/invoices/invoices.component.ts
+++ src/app/invoices/invoices.component.ts
@@ -1,20 +1,36 @@
 import { Component, Injectable } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faMagnifyingGlass, faCalendar } from '@fortawesome/free-solid-svg-icons';
-import { NgbDatepickerModule, NgbDateAdapter, NgbDateStruct, NgbDatepicker, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import {
+  faMagnifyingGlass,
+  faCalendar,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  NgbDatepickerModule,
+  NgbDateAdapter,
+  NgbDateStruct,
+  NgbDatepicker,
+  NgbCalendar,
+} from '@ng-bootstrap/ng-bootstrap';
 import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-invoices',
   standalone: true,
-  imports: [FontAwesomeModule, NgbDatepickerModule, FormsModule, JsonPipe, NgbDatepicker],
+  imports: [
+    FontAwesomeModule,
+    NgbDatepickerModule,
+    FormsModule,
+    JsonPipe,
+    NgbDatepicker,
+  ],
   templateUrl: './invoices.component.html',
   styleUrl: './invoices.component.css',
 })
-
 export class InvoicesComponent {
-
   constructor(library: FaIconLibrary) {
     library.addIcons(faMagnifyingGlass, faCalendar);
   }
@@ -22,4 +38,40 @@ export class InvoicesComponent {
   model: string;
   model2: string;
 
+  customerArray: any;
+
+  ngOnInit(): void {
+    this.customerArray = [
+      {
+        song: 'The Sliding Mr. Bones (Next Stop, Pottersville)',
+        artist: 'Malcolm Lockyer',
+        year: '1960'
+      },
+      {
+        song: 'Artist 2',
+        artist: 'Song 2',
+        year: '1980'
+      },
+      {
+        song: 'Artist 3',
+        artist: 'Song 3',
+        year: '1990'
+      },
+      {
+        song: 'Artist 4',
+        artist: 'Song 4',
+        year: '2010'
+      },
+      {
+        song: 'Artist 4',
+        artist: 'Song 4',
+        year: '2010'
+      },
+      {
+        song: 'Artist 4',
+        artist: 'Song 4',
+        year: '2010'
+      }
+    ];
+  }
 }
